870dbd256a5b89e0c4b3be3d63e650267a4b8c2d
kishan01052006 <kishan328125@gmail.com>
2026-02-22 19:21:21 +0530

    Changing backend url

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -24,7 +24,7 @@ const SignUp = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         sameSite: "lax",
-        secure: false, // true in production
+        secure: true, // true in production
         maxAge: 7 * 24 * 60 * 60 * 1000
       })
       .status(201)
